a70317545458b96bca88d7d24bad5f41b34f93aa
SungjinChoi <chltjdwls294@naver.com>
2026-04-11 17:02:07 +0900

    made selectsort

--- Sort.java
+++ Sort.java
@@ -1,11 +1,33 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Sort {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[5];
+        for(int i = 0; i <arr.length;i++)
+        {
+            arr[i] = sc.nextInt();
+        }
+        selectSortAsc(arr);
+        System.out.println(Arrays.toString(arr));
     }
     public static void Swap(int[] arr,int a,int b)
     {
         int temp = arr[a];
         arr[a] = arr[b];
         arr[b] = temp;
+    }
+    public static void selectSortAsc(int[] arr)
+    {
+        for(int i =0;i<arr.length;i++)
+        {
+            int minIndex = i;
+            for(int j = i+1;j<arr.length;j++)
+            {
+                if(arr[minIndex]>arr[j]) minIndex = j;
+            }
+            Swap(arr,minIndex,i);
+        }
     }
 }
