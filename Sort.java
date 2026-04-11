4e83783d62fda2bc3ea11352ee107f489d85db05
SungjinChoi <chltjdwls294@naver.com>
2026-04-11 16:58:45 +0900

    make method for Swap

--- Sort.java
+++ Sort.java
@@ -2,4 +2,10 @@ public class Sort {
     public static void main(String[] args) {
         System.out.println("hello world");
     }
+    public static void Swap(int[] arr,int a,int b)
+    {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
 }
